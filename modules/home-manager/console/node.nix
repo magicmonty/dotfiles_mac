f7bc19994dffc820427c9c4c850da428d089518d
Martin Gondermann <magicmonty@pagansoft.de>
2024-05-07 10:33:19 +0200

    Remove angular cli

--- modules/home-manager/console/node.nix
+++ modules/home-manager/console/node.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   config = {
-    home.packages = [
-      pkgs.nodePackages."@angular/cli"
-      pkgs.nodejs_20
+    home.packages = with pkgs; [
+      nodejs_20
+      yarn
     ];
   };
 }
