faea70f7b84a6099cc74c502572b2f340f8e4084
Martin Gondermann <martin.gondermann@bayoo.net>
2026-04-10 08:43:41 +0200

    Update flake

--- overlays/unstable/default.nix
+++ overlays/unstable/default.nix
@@ -1,4 +1,5 @@
 {channels, ...}: _final: _prev: {
   vimPlugins.vim-sensible = channels.unstable.vimPlugins.vim-sensible;
   inherit (channels.unstable) electron feishin;
+  direnv = _prev.direnv.overrideAttrs (_: {doCheck = false;});
 }
